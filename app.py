d30b92cad0b55341e291f8da7a810755c3caf1ca
José Polak <jose.polak@outlook.com>
2025-12-23 10:30:41 -0300

    Dia 03: rotas e respostas com HTML simples no Flask

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Olá, Flask!'
+    return '<h1>Relatório de Vendas</h1><p>Página inicial</p>'
+
+
+@app.route('/sobre')
+def sobre():
+    return '<h1>Sobre</h1><p>Projeto de estudo com Flask</p>'
+
+
+@app.route('/contato')
+def contato():
+    return '<h1>Contato</h1><p>Contato fictício</p>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-

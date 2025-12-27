c04cbaad65a3554d168da2c3fd4420663d78375f
José Polak <jose.polak@outlook.com>
2025-12-27 09:18:08 -0300

    Implementa cadastro manual de vendas via formulário

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+
 
 app = Flask(__name__)
 
@@ -26,6 +27,7 @@ def get_vendas():
     
     return vendas
 
+
 @app.route('/')
 def home():
     vendas = get_vendas()
@@ -37,6 +39,34 @@ def home():
         vendas = vendas,
         total = total
     )
+
+
+@app.route('/nova_venda')
+def nova_venda():
+    return render_template('nova_venda.html')
+
+
+@app.route('/processar_venda', methods = ['POST'])
+def processar_venda():
+    produto = request.form['produto']
+    quantidade = request.form['quantidade']
+    valor = request.form['valor']
+    
+    conn = sqlite3.connect('database/vendas.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO vendas (produto, quantidade, valor)
+        VALUES (?, ?, ?)
+        ''',
+        (produto, quantidade, valor)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run()
